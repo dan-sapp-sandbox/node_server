f960e29e57d4049396c85e897d13c6b7d68b79b9
Dan Sapp <dan.sapp@storable.com>
2024-11-01 20:49:57 -0500

    pokemon route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mockData = require("./testData");
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  console.log("GET /");
+  res.send("Express on Vercel")
+});
+app.get("/pokemon/", (req, res) => {
+  console.log("GET /pokemon");
+  res.send(JSON.stringify(mockData))
+});
 
 app.listen(5000, () => console.log("Server ready on port 5000."));
 
