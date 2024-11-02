514ef12760940172062963d0211d3ba0daf32651
Dan Sapp <dan.sapp@storable.com>
2024-11-01 20:38:00 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(5000, () => console.log("Server ready on port 5000."));
 
 module.exports = app;
