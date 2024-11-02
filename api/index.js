c5d76af4ec24a04dd91fbb723332c15373f57c5c
Dan Sapp <dan.sapp@storable.com>
2024-11-01 20:01:29 -0500

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,31 @@
-import cors from "cors";
-import express from "express";
+var express = require('express');
+var indexRouter = require('../routes/index');
+var app = express();
 
-const app = express();
+const whitelist = [
+  '*'
+];
 
-app.use(cors());
-app.use(express.json());
-app.get("/", (req, res) => {
-    res.send("Welcome to my server!");
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
 });
 
-const port = 2021;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
+app.use('/', indexRouter);
+
+module.exports = app;
